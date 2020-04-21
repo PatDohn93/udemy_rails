ab2b44ae9f077f5cbb0f60ff7ba4e981d948f4ac
PatDohn93 <dohnpatrick1993@gmail.com>
2020-04-21 13:49:53 -0400

    updates all files

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,6 +1,6 @@
-def cipher
+def cipher(rotation, password)
 alphabet = ("a".."z").to_a
-shifted_alphabet = ("a".."z").to_a.rotate(rotation.to_i)
+shifted_alphabet = ("a".."z").to_a.rotate(rotation)
 paired_alphabet = alphabet.zip(shifted_alphabet)
 encoder = Hash[paired_alphabet]
 
@@ -8,17 +8,6 @@ encoder = Hash[paired_alphabet]
     print encoder[key]
   end
 end
-
-def decipher
-  alphabet = ("a".."z").to_a
-  shifted_alphabet = ("a".."z").to_a.rotate( - rotation.to_i)
-  paired_alphabet = alphabet.zip(shifted_alphabet)
-  encoder = Hash[paired_alphabet]
-
-    password.chars.each do |key|
-      print encoder[key]
-    end
-  end
 
 
 
@@ -28,28 +17,17 @@ puts "Would you like to cipher or decipher?"
 user_input = gets.chomp.downcase
 
 if user_input == 'cipher'
-puts "What is the password?"
-password = gets.chomp.downcase
-puts "How many times would you like to shift?"
-rotation = gets.chomp
-alphabet = ("a".."z").to_a
-shifted_alphabet = ("a".."z").to_a.rotate(rotation.to_i)
-paired_alphabet = alphabet.zip(shifted_alphabet)
-encoder = Hash[paired_alphabet]
-  password.chars.each do |key|
-    print encoder[key]
-  end
-elsif user_input == 'decipher'
-   puts "What was the password?"
-   password = gets.chomp.downcase
-   puts "How many times was it shifted?"
-   rotation = gets.chomp
-   alphabet = ("a".."z").to_a
-   shifted_alphabet = ("a".."z").to_a.rotate( - rotation.to_i)
-   paired_alphabet = alphabet.zip(shifted_alphabet)
-   encoder = Hash[paired_alphabet]
+  puts "What is the password?"
+  password = gets.chomp.downcase
+  puts "How many times would you like to shift?"
+  rotation = gets.chomp.to_i
 
-     password.chars.each do |key|
-       print encoder[key]
-   end
+  cipher(rotation, password)
+elsif user_input == 'decipher'
+  puts "What was the password?"
+  password = gets.chomp
+  puts "How many times was it rotated?"
+  rotation = gets.chomp.to_i
+
+  cipher((- rotation), password)
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,23 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
 
+
+  @first_name
+  @last_name
+  @email
+  @username
+  @password
+
+    def initialize(firstname, lastname, email)
+        @first_name = firstname
+        @last_name = lastname
+        @email = email
+      end
+  def to_s
+    "first_name: #{first_name}, last_name #{@last_name}, email #{@email}"
+  end
+end
+
+patrick = Student.new("Patrick", "Dohn", "pat@email.com")
+
+puts patrick.last_name
